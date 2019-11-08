6c0d151ed06e91f03895035d1aeae65e58df29eb
Robert Hoekstra <robert.hoekstra@hva.nl>
2019-11-08 09:59:00 +0100

    code now cleans whole data and creates new data where needed

--- javascript/readcsv.js
+++ javascript/readcsv.js
@@ -1,22 +1,23 @@
 async function importDataset(){
     let dataset =  await d3.csv("../files/kleuren.csv")
 
-    // dataset.forEach(function(element){
-    //     element["Lievelingskleur (HEX code)"].split("#");
-    //  //   console.log(element)
-
-    // })
-
+   // Onderstaande code haalt alle waardes uit de data die niet overeenkomen met het "#000000" format. En past deze aan naar het juiste format. 
     let lijst = dataset
                 .map(element => {
 
-         let piet =  element["Lievelingskleur (HEX code)"];
-        // console.log(piet.replace("#","") )   
-        piet = piet.replace("#","").replace(" ","");
-        console.log(piet)
+         let kleurwaarde = element["Lievelingskleur (HEX code)"];
+        kleurwaarde = kleurwaarde.replace("#","").replace(" ","");
 
-        return element;
+
+        if (kleurwaarde.length == 6) {
+          } else {
+              kleurwaarde = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);}); 
+              
+              // dit stukje gejat van stackoverflow. Genereert een random hexcode terug zodat de totale data niet extreme waardes aanneemt.
+          }
+
+        return ("#" + kleurwaarde);  //voegt weer een # toe aan de hexreeks.
     })
-    //console.log(lijst)
+    console.log(lijst)
 }
 importDataset()
